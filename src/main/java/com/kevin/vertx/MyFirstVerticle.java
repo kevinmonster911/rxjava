0738f28b99d4bcdc0c54337a4e928cc3baacfe38
kevin <kevin@kevins-MacBook-Pro.local>
2016-10-23 20:17:56 +0800

    vertx

--- src/main/java/com/kevin/vertx/MyFirstVerticle.java
+++ src/main/java/com/kevin/vertx/MyFirstVerticle.java
@@ -2,18 +2,29 @@ package com.kevin.vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class MyFirstVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> startFuture) {
 
+
+        Router router = Router.router(vertx);
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response
+                    .putHeader("content-type", "text/html")
+                    .end("<h1>Hello from my first Vert.x 3 application</h1>");
+
+        });
+
         vertx.createHttpServer()
-                .requestHandler(r -> {
-                    r.response().end("<h1>Hello from my first " +
-                            "Vert.x 3 application</h1>");
-                })
-                .listen(8080, result -> {
+                .requestHandler(router::accept)
+                .listen(
+                    config().getInteger("http.port", 8080),
+                    result -> {
                     if(result.succeeded()) {
                         startFuture.complete();
                     } else {
